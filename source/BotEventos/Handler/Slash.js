a51108845fbc14d1d19c28233757f0cfe365ae31
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2024-01-03 21:59:29 -0300

    melhorias na sintaxe

--- source/BotEventos/Handler/Slash.js
+++ source/BotEventos/Handler/Slash.js
@@ -2,37 +2,24 @@ const client = require("../../../index");
 const dc = require("discord.js");
 const app = require('../../../app.json');
 
-client.on(`interactionCreate`, async(interaction) =>{
-
-  const Scomando = client.slashCommands.get(interaction.commandName);
-
+client.on(`interactionCreate`, async (interaction) => {
+  const command = client.slashCommands.get(interaction.commandName);
   if (interaction.type === 4) {
-    if(Scomando.autocomplete) {
+    if (command.autocomplete) {
       const choices = [];
-      await Scomando.autocomplete(interaction, choices)
+      await command.autocomplete(interaction, choices);
     }
   }
-
   if (!interaction.type === 2) return;
-
-  if(interaction.channel.type === 1) return interaction.reply({ content: `${app.emoji.nao} Meus comandos só funcionam em servidores.`, ephemeral: true})
-
-  if(!Scomando) return client.slashCommands.delete(interaction.commandName);
-
+  if (interaction.channel.type === 1) return interaction.reply({ content: `${app.emoji.nao} Meus comandos só funcionam em servidores.`, ephemeral: true })
+  if (!command) return client.slashCommands.delete(interaction.commandName);
   try {
-    
     interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
-    
-      Scomando.run(client, interaction, app);
-
-} catch(e) {
-
-  const e1 = new dc.EmbedBuilder()
-    .setDescription(`${app.emoji.nao} Não foi possível realizar este comando.`)
-    .setColor(app.bot.color)
-
+    command.run(client, interaction, app);
+  } catch (e) {
+    const e1 = new dc.EmbedBuilder()
+      .setDescription(`${app.emoji.nao} Não foi possível realizar este comando.`)
+      .setColor(app.bot.color)
     interaction.reply({ embeds: [e1] })
-
-}
-
+  }
 });
